d8a5ab47884163b31c54b4b7ae997f2b3290fd82
Nate Hardison <nate@cs.harvard.edu>
2012-06-18 15:27:51 -0400

    updating php files with namespace directives and dummy __construct

--- lib/check50/Check50.php
+++ lib/check50/Check50.php
@@ -1,8 +1,25 @@
 <?php
 
+namespace cs50\check50;
+
+/* initialization code here */
+
+spl_autoload_register(function($name) {
+    /* autoloading here */
+});
+
+/**
+ * This class is the main entry point for Check50. Clients should load this file
+ * with require_once and get an instance of this to then interact with the rest
+ * of the library.
+ */
 class Check50
 {
 
+    public function __construct()
+    {
+        /* empty for now */
+    }
 
 }
 
--- lib/check50/OutputScraper.php
+++ lib/check50/OutputScraper.php
@@ -1,8 +1,14 @@
 <?php
 
+namespace cs50\check50;
+
 class OutputScraper
 {
 
+    public function __construct()
+    {
+        /* implement me! */
+    }
 
 }
 
