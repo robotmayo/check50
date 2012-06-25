bedad13b54a9c85950feb069a1b7be6417dec356
Nate Hardison <nate@cs.harvard.edu>
2012-06-25 18:32:35 -0400

    Filling in with initial draft of pseudocode

--- lib/check50/Check50.php
+++ lib/check50/Check50.php
@@ -16,9 +16,51 @@ spl_autoload_register(function($name) {
 class Check50
 {
 
-    public function __construct()
+    /**
+     * Filename of source code to check.
+     *
+     * @var string $_src_code_filename
+     */
+    private $_src_code_filename;
+
+    /**
+     * Creates an instance of Check50 to run on the provided source code.
+     *
+     * @param string src_code_filename
+     */
+    public function __construct($src_code_filename)
     {
-        /* empty for now */
+        $this->_src_code_filename = $src_code_filename;
+    }
+
+    /**
+     * Runs Check50. 
+     *
+     * @param string test_suite
+     * @param array tests
+     *
+     * @return undecided
+     */
+    public function check($test_suite, array tests = NULL)
+    {
+        // Check what tests have been specified
+        // Pass code and tests to TestBuilder
+        // $tb = TestBuilder($this->src_code_filename);
+        // $built_tests = $tb->build_tests(tests);
+
+        // Pass built tests to run50
+        // $run50 = Run50Wrapper();
+        // $run50->upload($built_tests);
+        // $run50->run(function($args){
+        //     /* callback */
+        // });
+
+        // Pass results to TestScorer
+        // $ts = TestScorer();
+        // $ts->score();
+
+        // Return results to the user
+        // return $something;
     }
 
 }
