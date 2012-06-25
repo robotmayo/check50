a6d9682745b7ef996b3b2ee58037752101cea0c7
Nate Hardison <nate@cs.harvard.edu>
2012-06-25 18:42:29 -0400

    Adding simple config file with run50's URL (pulled from run50 git repo)

---
+++ etc/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * 
+ * Application-wide config variables and initialization procedures should go
+ * here.
+ *
+ * @author Nate Hardison
+ */
+
+$RUN50_URL = "http://ec2-50-16-142-191.compute-1.amazonaws.com:8080/run";
+
+?>
