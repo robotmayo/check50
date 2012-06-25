ca2920cbd0fc2223ce482e01a84179939bb2a035
Nate Hardison <nate@cs.harvard.edu>
2012-06-25 18:31:10 -0400

    Adding Run50 API module

---
+++ lib/check50/Run50API.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace cs50\check50;
+
+interface Run50API
+{
+
+    public function run();
+    public function upload();
+
+}
+
+
+?>
---
+++ lib/check50/Run50Wrapper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace cs50\check50;
+
+class Run50Wrapper implements Run50API
+{
+
+    public function __construct()
+    {
+        /* do something */
+    }
+
+    public function run()
+    {
+        
+    }
+
+    public function upload()
+    {
+
+    }
+
+}
+
+
+?>
