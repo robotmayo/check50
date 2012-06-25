21917b85a0d297efbbfd4e04330f828882ce86e9
Nate Hardison <nate@cs.harvard.edu>
2012-06-25 18:31:54 -0400

    Simple test scoring boilerplate

--- lib/check50/TestScorer.php
+++ lib/check50/TestScorer.php
@@ -1,15 +1,74 @@
 <?php
 
-namespace cs50\check50;
+/**
+ * @file
+ * 
+ * Module for parsing output and determining conformance to 
+ *
+ * @author Nate Hardison <nate@cs.harvard.edu>
+ * @package CS50
+ * @version 0.1
+ */
 
-class OutputScraper
+namespace CS50\Check50\TestScorer;
+
+/**
+ * Copies contents of $str into a temporary file in the system /tmp directory.
+ * Appends a newline to the file to keep diff happy. Returns the name of the
+ * temp file.
+ *
+ * @param string $str
+ *
+ * @return string
+ */
+function copy_to_temp_file($str)
 {
+    $tmp_filename = tempnam("/tmp", "");
 
-    public function __construct()
-    {
-        /* implement me! */
-    }
+    $tmp_file = fopen($tmp_filename, "w");
+    fwrite($tmp_file, $str);
+    fclose($tmp_file);
 
+    return $tmp_filename;
 }
+
+/**
+ * Diffs strings $a and $b with the options provided in the $options_array.
+ * Returns the result.
+ *
+ * @param string $a
+ * @param string $b
+ * @param array|string $options_array
+ *
+ * @return string
+ */
+function diff($a, $b, $options_array)
+{
+    // php diff libs don't coalesce whitespace, so use UNIX's diff
+    $tmp_a = copy_to_temp_file($a);
+    $tmp_b = copy_to_temp_file($b);
+
+    $result = shell_exec("diff " . implode(' ', $options_array) . $tmp_a . ' ' . $tmp_b);
+
+    unlink($tmp_a);
+    unlink($tmp_b);
+
+    return $result;
+}
+
+/**
+ *
+ * @param string $a
+ * @param string $b
+ * @param array|string $options_array
+ *
+ * @return string
+ */
+function diff_ignore_whitespace($a, $b, $options_array)
+{
+    return diff($a, $b, array_push($options_array, "-B", "-w");
+}
+
+
 
 ?>
