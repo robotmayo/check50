7ba660eea207f5ac3ec069a5b40af779aa3f0df7
Nate Hardison <nate@cs.harvard.edu>
2012-07-01 23:24:22 -0400

    Fixing indentation

--- lib/check50++/Test.cpp
+++ lib/check50++/Test.cpp
@@ -4,8 +4,8 @@
 
 namespace Check50 {
 
-Test::Test(const std::string& name):
-    _name(name),
+Test::Test(const std::string& name)
+  : _name(name),
     _notifiee(NULL),
     _source_code(NULL),
     _compile_directive(NULL),
@@ -15,38 +15,32 @@ Test::Test(const std::string& name):
     _received(0.0),
     _explanation(NULL) {}
 
-void
-Test::output_is(const std::string& output)
-{
-    _output = output;
+void Test::output_is(const std::string& output) {
+  _output = output;
 
-    if (_notifiee != NULL) {
-        // notifiee will scrape output
-        // and figure everything out
-        _notifiee->on_output(_output);
-    }
+  if (_notifiee != NULL) {
+    // notifiee will scrape output
+    // and figure everything out
+    _notifiee->on_output(_output);
+  }
 }
 
-void
-Test::received_is(Points received)
-{
-    _received = received;
+void Test::received_is(Points received) {
+  _received = received;
 
-    if (_explanation.empty() && _notifiee != NULL) {
-        _notifiee->on_complete();
-    }
+  if (_explanation.empty() && _notifiee != NULL) {
+    _notifiee->on_complete();
+  }
 }
 
-void
-Test::explanation_is(const std::string& explanation)
-{
-    _explanation = explanation;
+void Test::explanation_is(const std::string& explanation) {
+  _explanation = explanation;
 
-    // TODO: this won't work because _received is always true
-    // need to change somehow (different attribute?)
-    if (_received.value() != 0.0 && _notifiee != NULL) {
-        _notifiee->on_complete();
-    }
+  // TODO: this won't work because _received is always true
+  // need to change somehow (different attribute?)
+  if (_received.value() != 0.0 && _notifiee != NULL) {
+    _notifiee->on_complete();
+  }
 }
 
 } /* end namespace */
--- lib/check50++/Test.h
+++ lib/check50++/Test.h
@@ -13,81 +13,80 @@
 namespace Check50 {
 
 class Test : public Fwk::PtrInterface<Test> {
+ public:
+  typedef Fwk::Ptr<Test> Ptr;
 
-public:
-    typedef Fwk::Ptr<Test> Ptr;
+  std::string name() const { return _name; }
 
-    std::string name() const { return _name; }
+  class Notifiee : public Fwk::BaseNotifiee<Test> {
+  public:
+    typedef Fwk::Ptr<Notifiee> Ptr;
 
-    class Notifiee : public Fwk::BaseNotifiee<Test> {
-    public:
-        typedef Fwk::Ptr<Notifiee> Ptr;
+    virtual void on_output(const std::string& output) {}
+    virtual void on_complete() {}
 
-        virtual void on_output(const std::string& output) {}
-        virtual void on_complete() {}
+    Notifiee(Test *test) : Fwk::BaseNotifiee<Test>(test) {}
+  };
 
-        Notifiee(Test *test) : Fwk::BaseNotifiee<Test>(test) {}
-    };
+  Fwk::Ptr<Notifiee> notifiee() const { return _notifiee; }
+  void last_notifiee_is(Notifiee *n) {
+    Test *me = const_cast<Test *>(this);
+    me->_notifiee = n;
+  }
 
-    Fwk::Ptr<Notifiee> notifiee() const { return _notifiee; }
-    void last_notifiee_is(Notifiee *n) {
-        Test *me = const_cast<Test *>(this);
-        me->_notifiee = n;
-    }
+  SourceCode::Ptr source_code() const { return _source_code; }
+  virtual void source_code_is(const SourceCode::Ptr source_code) {
+    _source_code = source_code;
+  }
 
-    SourceCode::Ptr source_code() const { return _source_code; }
-    virtual void source_code_is(const SourceCode::Ptr source_code) {
-        _source_code = source_code;
-    }
+  std::string compile_directive() const { return _compile_directive; }
+  virtual void compile_directive_is(const std::string& compile_directive) {
+    _compile_directive = compile_directive;
+  }
 
-    std::string compile_directive() const { return _compile_directive; }
-    virtual void compile_directive_is(const std::string& compile_directive) {
-        _compile_directive = compile_directive;
-    }
+  std::string execution_directive() const { return _execution_directive; }
+  virtual void execution_directive_is(const std::string& execution_directive) {
+    _execution_directive = execution_directive;
+  }
 
-    std::string execution_directive() const { return _execution_directive; }
-    virtual void execution_directive_is(const std::string& execution_directive) {
-        _execution_directive = execution_directive;
-    }
+  Points possible() const { return _possible; }
+  virtual void possible_is(Points possible) {
+    _possible = possible;
+  }
 
-    Points possible() const { return _possible; }
-    virtual void possible_is(Points possible) {
-        _possible = possible;
-    }
+  std::string output() const { return _output; }
+  virtual void output_is(const std::string& output);
 
-    std::string output() const { return _output; }
-    virtual void output_is(const std::string& output);
+  Points received() const { return _received; }
+  virtual void received_is(Points received);
 
-    Points received() const { return _received; }
-    virtual void received_is(Points received);
+  std::string explanation() const { return _explanation; }
+  virtual void explanation_is(const std::string& explanation);
 
-    std::string explanation() const { return _explanation; }
-    virtual void explanation_is(const std::string& explanation);
+  static Test::Ptr TestNew(const std::string& name) {
+    Ptr t = new Test(name);
+    return t;
+  }
 
-    static Test::Ptr TestNew(const std::string& name) {
-        Ptr t = new Test(name);
-        return t;
-    }
+ protected:
+  Test(const std::string& name);
+  Test(const Test&);
 
-protected:
-    Test(const std::string& name);
-    Test(const Test&);
+ private:
+  std::string _name;
+  Notifiee *_notifiee;
 
-private:
-    std::string _name;
-    Notifiee *_notifiee;
+  SourceCode::Ptr _source_code;
 
-    SourceCode::Ptr _source_code;
+  // the following are read from the file
+  std::string _compile_directive;
+  std::string _execution_directive;
+  Points _possible;
 
-    // the following are read from the file
-    std::string _compile_directive;
-    std::string _execution_directive;
-    Points _possible;
-
-    // the following are determined by the test's execution
-    std::string _output;
-    Points _received;
-    std::string _explanation;
+  // the following are determined by the test's execution
+  std::string _output;
+  Points _received;
+  std::string _explanation;
 };
 
 }      /* end namespace Check50 */
