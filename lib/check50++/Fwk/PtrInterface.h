39a36e767f42627052ebaed4d50b6e10d45eeaac
Nate Hardison <nate@cs.harvard.edu>
2012-07-01 22:50:08 -0400

    Fixing framework dir

--- lib/check50++/Fwk/PtrInterface.h
+++ lib/check50++/Fwk/PtrInterface.h
@@ -5,16 +5,18 @@ namespace Fwk {
 
 template <class T>
 class PtrInterface {
-public:
-    PtrInterface() : _ref(0) {}
-    unsigned long references() const { return _ref; }
-    inline const PtrInterface * new_ref() const { ++_ref; return this; }
-    inline void delete_ref() const { if( --_ref == 0 ) on_zero_references(); }
-protected:
-    virtual ~PtrInterface() {}
-    virtual void on_zero_references() const { delete this; }
-private:
-    mutable long unsigned _ref;
+ public:
+  PtrInterface() : _ref(0) {}
+  unsigned long references() const { return _ref; }
+  inline const PtrInterface * new_ref() const { ++_ref; return this; }
+  inline void delete_ref() const { if( --_ref == 0 ) on_zero_references(); }
+
+ protected:
+  virtual ~PtrInterface() {}
+  virtual void on_zero_references() const { delete this; }
+
+ private:
+  mutable long unsigned _ref;
 };
 
 }
