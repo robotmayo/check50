90eb8e23f74a9125d433f5dd4d45f7d6bb93820b
Nate Hardison <nate@cs.harvard.edu>
2012-07-02 00:52:51 -0400

    Adding in a shell for testing

--- lib/check50++/TestFileReader.cpp
+++ lib/check50++/TestFileReader.cpp
@@ -7,10 +7,20 @@
 namespace Check50 {
     
 Test::Ptr YamlTestFileReader::test_new(const std::string& test_filename) {
+  Test::Ptr test = Test::TestNew(test_filename);
+
   // parse the YAML test file
   // ensure it has the appropriate extension
-  // find a good parsing library
-  throw new Fwk::NoImplementationException("No implementation yet!");
+  // let's use one of these libraries:
+  // 1) http://pyyaml.org/wiki/LibYAML
+  // 2) http://code.google.com/p/yaml-cpp/
+  //throw new Fwk::NoImplementationException("No implementation yet!");
+
+  // test->compile_directive_is();
+  // test->execution_directive_is();
+  // test->possible(Points(2));
+
+  return test;
 }
 
 }
