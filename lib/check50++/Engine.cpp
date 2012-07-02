5edaed8836ba0903f511dec1e352c1b761b4203d
Nate Hardison <nate@cs.harvard.edu>
2012-07-01 23:18:44 -0400

    Fixing indentation

--- lib/check50++/Engine.cpp
+++ lib/check50++/Engine.cpp
@@ -23,9 +23,9 @@ Engine::Engine(const std::string& name,
     _test_file_format(test_file_format),
     _activity_manager(ActivityManager::ActivityManagerNew()) {
 
-    if (test_file_format == Engine::yaml) {
-        _test_file_reader = YamlTestFileReader::YamlTestFileReaderNew("name");   
-    }
+  if (test_file_format == Engine::yaml) {
+    _test_file_reader = YamlTestFileReader::YamlTestFileReaderNew("name");   
+  }
 }
 
 Test::Ptr Engine::test(const std::string& test_filename) const {
@@ -40,35 +40,35 @@ Test::Ptr Engine::test(const std::string& test_filename) const {
 }
 
 Test::Ptr Engine::test_new(const std::string& test_filename) {
-    if (_test[test_filename]) {
-        throw Fwk::NameInUseException("Already have \"" + test_filename + "\"");
-    }
+  if (_test[test_filename]) {
+    throw Fwk::NameInUseException("Already have \"" + test_filename + "\"");
+  }
 
-    Test::Ptr test;
+  Test::Ptr test;
     
-    try {
-        test = _test_file_reader->test_new(test_filename);
-    } catch (Fwk::Exception e) {
-        // TODO: fix what we're catching here
-    }
+  try {
+    test = _test_file_reader->test_new(test_filename);
+  } catch (Fwk::Exception e) {
+    // TODO: fix what we're catching here
+  }
 
-    test->source_code_is(_source_code);
+  test->source_code_is(_source_code);
 
-    // build up an execution activity
-    Activity::Ptr execution_activity = _activity_manager->activity_new(test->name());
-    execution_activity->last_notifiee_is(new ExecutionActivityReactor(execution_activity.ptr(), _activity_manager, test));
-    execution_activity->status_is(Activity::waiting);
+  // build up an execution activity
+  Activity::Ptr execution_activity = _activity_manager->activity_new(test->name());
+  execution_activity->last_notifiee_is(new ExecutionActivityReactor(execution_activity.ptr(), _activity_manager, test));
+  execution_activity->status_is(Activity::waiting);
 
-    _test[test_filename] = test;
+  _test[test_filename] = test;
 
-    return test;
+  return test;
 }
 
 void Engine::test_del(const std::string& name) {
-    std::map<std::string, Test::Ptr>::iterator it = _test.find(name);
-    if (it == _test.end()) return;
+  std::map<std::string, Test::Ptr>::iterator it = _test.find(name);
+  if (it == _test.end()) return;
 
-    _test.erase(it);
+  _test.erase(it);
 }
 
 } /* end namespace */
--- lib/check50++/Engine.h
+++ lib/check50++/Engine.h
@@ -16,85 +16,85 @@
 namespace Check50 {
 
 class Engine : public Fwk::PtrInterface<Engine> {
-public:
-    typedef Fwk::Ptr<Engine> Ptr;
+ public:
+  typedef Fwk::Ptr<Engine> Ptr;
 
-    std::string name() const { return _name; }
+  std::string name() const { return _name; }
 
-    // the Engine's notifiee wants to hear about new test results
-    // TODO: figure out if this is indeed the best way
-    class Notifiee : public Fwk::BaseNotifiee<Engine> {
-    public:
-        typedef Fwk::Ptr<Notifiee> Ptr;
+  // the Engine's notifiee wants to hear about new test results
+  // TODO: figure out if this is indeed the best way
+  class Notifiee : public Fwk::BaseNotifiee<Engine> {
+  public:
+    typedef Fwk::Ptr<Notifiee> Ptr;
 
-        virtual void on_test_complete(Test::Ptr test);
+    virtual void on_test_complete(Test::Ptr test);
 
-        Notifiee(Engine *engine) : Fwk::BaseNotifiee<Engine>(engine) {}
-    };
+    Notifiee(Engine *engine) : Fwk::BaseNotifiee<Engine>(engine) {}
+  };
 
-    Fwk::Ptr<Notifiee> notifiee() { return _notifiee; }
-    void last_notifiee_is(Notifiee *n) {
-        Engine *me = const_cast<Engine *>(this);
-        me->_notifiee = n;
-    }
+  Fwk::Ptr<Notifiee> notifiee() { return _notifiee; }
+  void last_notifiee_is(Notifiee *n) {
+    Engine *me = const_cast<Engine *>(this);
+    me->_notifiee = n;
+  }
 
-    // TODO: are these necessary?
-    enum EngineMode {
-        development,
-        production
-    };
-    EngineMode engine_mode() const { return _engine_mode; }
-    void engine_mode_is(EngineMode engine_mode) {
-        _engine_mode = engine_mode;
-    }
+  // TODO: are these necessary?
+  enum EngineMode {
+    development,
+    production
+  };
+  EngineMode engine_mode() const { return _engine_mode; }
+  void engine_mode_is(EngineMode engine_mode) {
+    _engine_mode = engine_mode;
+  }
 
-    SourceCode::Ptr source_code() const { return _source_code; }
+  SourceCode::Ptr source_code() const { return _source_code; }
 
-    // TODO: what other formats do we want to support?
-    enum TestFileFormat {
-        yaml
-        /* add other formats here and a corresponding static accessor below */
-    };
-    TestFileFormat test_file_format() const { return _test_file_format; }
+  // TODO: what other formats do we want to support?
+  enum TestFileFormat {
+    yaml
+    /* add other formats here and a corresponding static accessor below */
+  };
+  TestFileFormat test_file_format() const { return _test_file_format; }
 
 
-    Test::Ptr test(const std::string& test_filename) const;
-    unsigned int tests() const { return _test.size(); }
+  Test::Ptr test(const std::string& test_filename) const;
+  unsigned int tests() const { return _test.size(); }
 
-    // test_is ensures test exists, test_new throws an exception if it does.
-    // TODO: are they both necessary?
-    Test::Ptr test_is(const std::string& test_filename);
-    Test::Ptr test_new(const std::string& test_filename);
+  // test_is ensures test exists, test_new throws an exception if it does.
+  // TODO: are they both necessary?
+  Test::Ptr test_is(const std::string& test_filename);
+  Test::Ptr test_new(const std::string& test_filename);
  
-    // TODO: is this necessary?
-    void test_del(const std::string& test_filename);
+  // TODO: is this necessary?
+  void test_del(const std::string& test_filename);
 
-    // this is the general way to create an Engine
-    static Engine::Ptr EngineNew(const std::string& name,
-                                 std::string& source_code_file,
-                                 EngineMode& engine_mode,
-                                 TestFileFormat& test_file_format) {
-        Ptr e = new Engine(name, source_code_file, engine_mode, test_file_format);
-        return e;
-    }
+  // this is the general way to create an Engine
+  static Engine::Ptr EngineNew(const std::string& name,
+                               std::string& source_code_file,
+                               EngineMode& engine_mode,
+                               TestFileFormat& test_file_format) {
+    Ptr e = new Engine(name, source_code_file, engine_mode, test_file_format);
+    return e;
+  }
 
-protected:
-    // defined in Engine.cpp
-    Engine(const std::string& name, std::string& source_code_file,
-           EngineMode engine_mode, TestFileFormat test_file_format);
+ protected:
+  // defined in Engine.cpp
+  Engine(const std::string& name, std::string& source_code_file,
+         EngineMode engine_mode, TestFileFormat test_file_format);
 
-    // leave undefined so that it can't be copied
-    Engine(const Engine&);
+  // leave undefined so that it can't be copied
+  Engine(const Engine&);
 
-private:
-    std::string _name;
-    SourceCode::Ptr _source_code;
-    Notifiee *_notifiee;
-    EngineMode _engine_mode;
-    TestFileFormat _test_file_format;
-    TestFileReader::Ptr _test_file_reader;
-    std::map<std::string, Test::Ptr> _test;
-    ActivityManager::Ptr _activity_manager;
+ private:
+  std::string _name;
+  SourceCode::Ptr _source_code;
+  Notifiee *_notifiee;
+  EngineMode _engine_mode;
+  TestFileFormat _test_file_format;
+  TestFileReader::Ptr _test_file_reader;
+  std::map<std::string, Test::Ptr> _test;
+  ActivityManager::Ptr _activity_manager;
 };
 
 } /* end namespace */
